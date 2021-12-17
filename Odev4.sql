648ce9482c1e0ab50d55063e7229acf14992357b
Kaan Lokum <49822875+klokum@users.noreply.github.com>
2021-12-17 23:08:32 +0300

    Update Odev4.sql

--- Odev4.sql
+++ Odev4.sql
@@ -10,7 +10,7 @@ SELECT COUNT(DISTINCT replacement_cost) FROM film;
 
 --3.film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 
-SELECT COUNT(title)  FROM film
+SELECT COUNT(title) FROM film
 WHERE title LIKE 'T%' AND rating = 'G';
 
 --4.country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
